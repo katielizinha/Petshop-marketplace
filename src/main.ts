b1d6c8a2012b2d54a01b6139e1e42344bbe6f2ae
katielizinha <116743082+katielizinha@users.noreply.github.com>
2024-11-26 11:43:03 -0400

    Nicolle - Adicionando a conexão com a Tabela consulta

--- src/main.ts
+++ src/main.ts
@@ -81,6 +81,23 @@ app.get("/funcionarios", async (req, res) => {
     }
 })
 
+app.get("/consulta", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "test",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const [result, fields] = await connection.query("SELECT * from consulta")
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send("Server ERROR")
+    }
+})
 
 app.listen(8000, () => {
     console.log("Iniciei o servidor")
