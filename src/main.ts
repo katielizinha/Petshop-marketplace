cf338ec4d1144b3fe6f9f26f8a32ffb1d01fc46b
katielizinha <katiely.santos@estudante.ifms.edu.br>
2024-11-30 06:25:50 -0400

    Katiély- Arrumando a conexão com a tabela animais e donos

--- src/main.ts
+++ src/main.ts
@@ -82,7 +82,7 @@ app.get("/consulta", async (req, res) => {
     }
 })
 
-app.get("/animal", async (req, res) => {
+app.get("/animais", async (req, res) => {
     try {
         const connection = await mysql.createConnection({
             host: process.env.dbhost ? process.env.dbhost : "localhost",
@@ -100,6 +100,44 @@ app.get("/animal", async (req, res) => {
     }
 })
 
+app.get("/donos", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "test",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const [result, fields] = await connection.query("SELECT * from donos")
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send("Server ERROR")
+    }
+})
+
+app.post("/donos", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "teste",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const {id,nomeDono,nomeAnimal,cpf,telefone,dataCadastro} = req.body
+        const [result, fields] = 
+                    await connection.query("INSERT INTO donos VALUES (?,?,?,?,?,?)",
+                            [id,nomeDono,nomeAnimal,cpf,telefone,dataCadastro])
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
 app.listen(8000, () => {
     console.log("Iniciei o servidor")
 })
