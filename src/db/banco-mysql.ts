1b54c85ec17033545feb7367ebfde3a3d37fc854
katielizinha <116743082+katielizinha@users.noreply.github.com>
2025-02-04 08:49:29 -0400

    Arrumando o banco de dados

--- src/db/banco-mysql.ts
+++ src/db/banco-mysql.ts
@@ -28,9 +28,9 @@ class BancoMysql{
         const [result, fields] = await this.connection.query("SELECT * FROM produtos")
         return result
     }
-    async inserir(produto:{id:number,nome:string,descricao:string,preco:string,imagem:string}){
+    async inserir(produto:{id:number,nome:string,descricao:string,preco:string,imagem:string, data_producao:Date}){
         if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
-        const [result, fields] = await this.connection.query("INSERT INTO produtos VALUES (?,?,?,?,?)",[produto.id,produto.nome,produto.descricao,produto.preco,produto.imagem])
+        const [result, fields] = await this.connection.query("INSERT INTO produtos VALUES (?,?,?,?,?,?)",[produto.id,produto.nome,produto.descricao,produto.preco,produto.imagem, produto.data_producao])
         return result
     }
     async excluir(id:string){
@@ -38,9 +38,9 @@ class BancoMysql{
         const [result, fields] = await this.connection.query("DELETE FROM produtos WHERE id = ?",[id])
         return result
     }
-    async alterar(id:string,produto:{id?:string,nome:string,descricao:string,preco:string,imagem:string}){
+    async alterar(id:string,produto:{id?:string,nome:string,descricao:string,preco:string,imagem:string, data_producao:Date}){
         if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
-        const [result, fields] = await this.connection.query("UPDATE produtos SET nome=?,descricao=?,preco=?,imagem=? WHERE id=?",[produto.nome,produto.descricao,produto.preco,produto.imagem,id])
+        const [result, fields] = await this.connection.query("UPDATE produtos SET nome=?,descricao=?,preco=?,imagem=?, data_producao=? WHERE id=?",[produto.nome,produto.descricao,produto.preco,produto.imagem, produto.data_producao, id])
         return result
     }
     async listarPorId(id:string){
@@ -68,11 +68,9 @@ class BancoMysql{
         const [result, fields] = await this.connection.query("DELETE FROM donos WHERE id = ?",[id])
         return result
     }
-    async alterarDonos(id: string, dono: { id?: string, nomeDono: string, nomeAnimal: string, CPF: string, telefone: string, dataCadastro: string, imagem: string }) {
+    async alterarDonos(id: string, dono: { id?:string, nomeDono:string, nomeAnimal:string, CPF:string, telefone:string, dataCadastro:Date, imagem:string }) {
         if (!this.connection) throw new Error("Erro de conexão com o banco de dados.");
-        const [result, fields] = await this.connection.query(
-            "UPDATE donos SET nomeDono=?, nomeAnimal=?, CPF=?, telefone=?, dataCadastro=?, imagem=? WHERE id=?",
-            [dono.nomeDono, dono.nomeAnimal, dono.CPF, dono.telefone, dono.dataCadastro, dono.imagem, id]
+        const [result, fields] = await this.connection.query("UPDATE donos SET nomeDono=?, nomeAnimal=?, CPF=?, telefone=?, dataCadastro=?, imagem=? WHERE id=?",[dono.nomeDono, dono.nomeAnimal, dono.CPF, dono.telefone, dono.dataCadastro, dono.imagem, id]
         );
         return result;
     }
